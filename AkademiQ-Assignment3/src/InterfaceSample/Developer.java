eae5cee8489a94ba938d43a195fab93feb411e23
Berat Ölmez <159007239+seberatolmez@users.noreply.github.com>
2025-03-09 08:15:21 +0300

    Updated Developer.java

--- AkademiQ-Assignment3/src/InterfaceSample/Developer.java
+++ AkademiQ-Assignment3/src/InterfaceSample/Developer.java
@@ -4,6 +4,7 @@ public class Developer extends Employee{
     private String about;
     public Developer(int id, String name, double salary, String department,String about) {
         super(id, name, salary, department);
+         this.about = about ;
     }
 
 
